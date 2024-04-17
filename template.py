4e3d1f9a7e838f4f78c517db02728eb5a845ce3c
bharath_varala <bharathvarala11@gmail.com>
2024-04-17 11:17:40 +0530

    changes

--- template.py
+++ template.py
@@ -1,2 +1,17 @@
+#for all math related things lcm,gcd,primes
 import math 
+
 from collections import Counter,deque
+#for dict of count deque to add and delete elements from both sides 
+
+import heapq as h
+# pq=[]
+# h.heapify(pq)
+# h.heappush(pq,(2,3))
+# print(h.heappop(pq))
+
+from sortedcontainers import SortedList, SortedSet, SortedDict
+# s=SortedList()
+# s.add(1)
+
+
