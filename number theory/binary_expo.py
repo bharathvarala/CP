ab5a163355cb55c0ad5a5c675d3d9f75e9cbfe5c
bharath_varala <bharathvarala11@gmail.com>
2024-04-29 11:13:40 +0530

    number theory

--- number theory/binary_expo.py
+++ number theory/binary_expo.py
@@ -7,4 +7,19 @@ def power(a,n):
     if n&1==0:
         return power(a,n//2)*power(a,n//2)
     return a*power(a,n//2)*power(a,n//2)
-print(power(2,3))
+print(power(5,4))
+
+#iterative approach 
+
+ans=1
+a=5
+n=4
+while n:
+    if n&1:
+        ans*=a
+        n-=1
+    else:
+        a*=a
+        n//=2 
+print(ans) 
+    
